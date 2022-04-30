2981e0da38147b3bf7eb193a372b7dccd88b8152
anaomart <37917804+anaomart@users.noreply.github.com>
2022-04-30 13:11:08 +0200

    Update FindTheOddInt.js

--- kata-6/FindTheOddInt.js
+++ kata-6/FindTheOddInt.js
@@ -15,12 +15,4 @@ function findOdd(A) {
 
 }
 
-function findOdd2(A) {
-    for (let i = 0; i < A.length; i++) {
-        let len = A.lastIndexOf(A[i]) - A.indexOf(A[i]);
-    }
-
-
-
-}
 console.log(findOdd([1, 1, 3, 7, 2, 3, 2, 4, 4, 7]))
